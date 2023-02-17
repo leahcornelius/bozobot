250005d7cf2e72df03fd0ea0cf6431e952081d48
Leo Cornelius <leocornelius@gmail.com>
2023-02-17 20:49:55 +0000

    Launch cam stream with alunch file

--- src/bozobot/launch/robot.launch.py
+++ src/bozobot/launch/robot.launch.py
@@ -9,5 +9,21 @@ def generate_launch_description():
             executable='telop_subscriber',
             name='telop_subscriber', 
         ),
-        
+        # RosUsbCam
+        Node(
+            package='usb_cam',
+            namespace='bozobot',
+            executable='usb_cam_node',
+            name='usb_cam_node',
+            parameters=[
+                {'video_device': '/dev/video0'},
+                {'image_width': 640},
+                {'image_height': 480},
+                {'pixel_format': 'yuyv'},
+                {'camera_frame_id': 'camera'},
+                {'io_method': 'mmap'},
+                {'image_pub_rate': 30},
+                {'camera_name': 'bozobot'},
+            ]
+        ),
     ])
