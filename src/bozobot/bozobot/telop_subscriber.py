03b38ca0f3f8839f035214e0e8f16f19b36dd616
Leo Cornelius <leocornelius@gmail.com>
2023-02-17 20:36:12 +0000

    Log debug messages from telop

--- src/bozobot/bozobot/telop_subscriber.py
+++ src/bozobot/bozobot/telop_subscriber.py
@@ -9,6 +9,7 @@ from bozobot.motor import MotorDriver, MotorDriverCommand
 class TelopSubscriber(Node):
     def __init__(self):
         super().__init__('TelopSubscriber')
+        self.get_logger().set_level(rclpy.logging.LoggingSeverity.DEBUG)
         self.subscription = self.create_subscription(
             Twist,
             'cmd_vel',
